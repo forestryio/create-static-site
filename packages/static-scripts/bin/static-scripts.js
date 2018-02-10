329a77184e40b905dc33ebeab7f5d0929ef8a547
ncphillips <ncphillips.19@gmail.com>
2018-02-10 10:40:16 -0400

    refactor static-scripts to be a bit more organized

--- packages/static-scripts/bin/static-scripts.js
+++ packages/static-scripts/bin/static-scripts.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 /**
  * Copyright (c) 2015-present, Facebook, Inc.
  *
@@ -13,23 +14,34 @@ process.on("unhandledRejection", err => {
   throw err
 })
 
-const spawn = require("react-dev-utils/crossSpawn")
-const args = process.argv.slice(2)
-
-const scriptIndex = args.findIndex(x => x === "build" || x === "start")
-const script = scriptIndex === -1 ? args[0] : args[scriptIndex]
-const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : []
-const uhh = {
-  result: null,
+// CONSOLE OUTPUT MESSAGES
+const MESSAGES = {
+  SIGKILL: () =>
+    "The build failed because the process exited too early. " +
+    "This probably means the system ran out of memory or someone called " +
+    "`kill -9` on the process.",
+  SIGTERM: () =>
+    "The build failed because the process exited too early. " +
+    "Someone might have called `kill` or `killall`, or the system could " +
+    "be shutting down.",
+  SIGNULL: () =>
+    "The build failed because the process exited too early. " +
+    "It is unclear why this happened.",
+  UNKNOWN_SCRIPT: script =>
+    `Unknown script "${script}".\n` +
+    "Perhaps you need to update static-scripts?\n" +
+    "See: https://github.com/forestryio/create-static-site/blob/master/packages/static-scripts/template/README.md#updating-to-new-releases",
 }
 
+// SCRIPTS
 const gulpArgs = [
   "--gulpfile",
   "node_modules/static-scripts/scripts/gulpFile.js",
   "--cwd",
   ".",
 ]
-const scripts = {
+
+const SPAWN_SCRIPTS = {
   start: ["gulp", [...gulpArgs, "server"]],
   preview: [
     "cross-env",
@@ -44,38 +56,35 @@ const scripts = {
   build: ["cross-env", ["NODE_ENV=production", "gulp", ...gulpArgs, "build"]],
 }
 
-switch (script) {
-  case "build":
-  case "preview":
-  case "start":
-    uhh.result = spawn.sync(...scripts[script], { stdio: "inherit" })
-    break
-  case "eject":
-  default:
-    console.log('Unknown script "' + script + '".')
-    console.log("Perhaps you need to update static-scripts?")
-    console.log(
-      "See: https://github.com/forestryio/create-static-site/blob/master/packages/static-scripts/template/README.md#updating-to-new-releases"
-    )
-    break
-}
+// HANDLE ARGS & RUN THE SCRIPT
+const spawn = require("react-dev-utils/crossSpawn")
+const args = process.argv.slice(2)
 
-if (uhh.result) {
-  if (uhh.result.signal) {
-    if (uhh.result.signal === "SIGKILL") {
-      console.log(
-        "The build failed because the process exited too early. " +
-          "This probably means the system ran out of memory or someone called " +
-          "`kill -9` on the process."
-      )
-    } else if (uhh.result.signal === "SIGTERM") {
-      console.log(
-        "The build failed because the process exited too early. " +
-          "Someone might have called `kill` or `killall`, or the system could " +
-          "be shutting down."
-      )
-    }
+const scriptIndex = args.findIndex(x => x === "build" || x === "start")
+const script = scriptIndex === -1 ? args[0] : args[scriptIndex]
+const result = runScript(script)
+
+if (result) {
+  if (result.signal) {
+    const terminationMessage = MESSAGES[result.signal] || MESSAGES.SIGNULL
+    console.log(terminationMessage())
     process.exit(1)
   }
-  process.exit(uhh.result.status)
+  process.exit(result.status)
+}
+
+/**
+ * runScript(script)
+ */
+function runScript(script) {
+  switch (script) {
+    case "build":
+    case "preview":
+    case "start":
+      return spawn.sync(...SPAWN_SCRIPTS[script], { stdio: "inherit" })
+    case "eject":
+    default:
+      console.log(MESSAGES.UNKNOWN_SCRIPT(script))
+      break
+  }
 }
